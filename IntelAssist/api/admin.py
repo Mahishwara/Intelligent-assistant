d1441c81b093c833a4b6e13e762e43dc9987f30e
Mahishwara <kmisha043@mail.ru>
2023-11-27 20:34:36 +0500

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	.idea/vcs.xml

--- IntelAssist/api/admin.py
+++ IntelAssist/api/admin.py
@@ -3,6 +3,7 @@ from .models import *
 # Register your models here.
 # Misha loh
 # Misha molodez
+# Misha
 admin.site.register(User)
 admin.site.register(BankAccount)
 admin.site.register(Transaction)
