9c7caba1b9acfadfe5f614479b73c52453856cbc
Mahishwara <kmisha043@mail.ru>
2023-11-27 11:23:47 +0500

    Merge remote-tracking branch 'github/main'
    # Conflicts:
    #	.idea/.gitignore
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	.idea/vcs.xml

--- IntelAssist/api/views.py
+++ IntelAssist/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
+# Тестовый комментарий для PUSH
 
 @api_view(['GET'])
 def getRoutes(request):
